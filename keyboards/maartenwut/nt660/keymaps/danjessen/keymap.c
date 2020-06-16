fc5ac2b0e3be2083c0496008b23bee20fa8f6def
Dan Jessen <danjessen@gmail.com>
2020-06-16 16:06:19 +0200

    fixed layout

--- keyboards/maartenwut/nt660/keymaps/danjessen/keymap.c
+++ keyboards/maartenwut/nt660/keymaps/danjessen/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "../../../../../quantum/keymap_extras/keymap_us_international.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -25,13 +26,14 @@
 #define _SP 3
 
 #define SPACE LT(_SP, KC_SPC)
+#define SHIFT
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_MA] = LAYOUT_all(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_MPLY,
+  KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_MPLY,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,
-  KC_TILD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  _______, KC_ENT,
+  KC_P5,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  _______, KC_ENT,
   KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,
   KC_LCTL, KC_LGUI, KC_LALT,                   SPACE,                              KC_RALT, KC_RCTRL,MO(_FL),  KC_LEFT, KC_DOWN, KC_RGHT),
 
@@ -44,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FL] = LAYOUT_all(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  RESET,   KC_PSCR,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_INS,
-  _______, _______, _______, _______, _______, TG(_GA), _______, _______, _______, _______, _______, _______,  _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,  _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, US_OSTR, _______, _______,  _______, _______, KC_INS,
+  _______, US_ARNG, _______, _______, _______, TG(_GA), _______, _______, _______, _______, _______, _______,  _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, US_AE,    _______, _______,
   _______, _______, _______,                   _______,                            _______, _______, _______,  _______, _______, _______),
 
 [_SP] = LAYOUT_all(
