6e380b02250910d57c3be2172b2bda1597447651
Dan Jessen <danjessen@gmail.com>
2020-06-29 10:05:47 +0200

    move US_AE and remapped capslock

--- keyboards/maartenwut/nt660/keymaps/danjessen/keymap.c
+++ keyboards/maartenwut/nt660/keymaps/danjessen/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MA] = LAYOUT_all(
   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_MPLY,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,
-  KC_TILD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  _______, KC_ENT,
+  KC_DLR , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  _______, KC_ENT,
   KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,
   KC_LCTL, KC_LGUI, KC_LALT,                   SPACE,                              KC_RALT, KC_RCTRL,MO(_FL),  KC_LEFT, KC_DOWN, KC_RGHT),
 
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SP] = LAYOUT_all(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  _______, KC__VOLUP,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   US_OSTR, _______, _______,  _______, _______, KC__VOLDOWN,
-  _______, US_ARNG, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  US_AE,   _______,
+  _______, US_ARNG, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, US_AE  ,  _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
   _______, _______, _______,                   _______,                            _______, _______, _______,  _______, _______, _______),
 };
